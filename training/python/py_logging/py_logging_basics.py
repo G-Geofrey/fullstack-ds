c85d41bb0c1c1e4f64fbe2f5173f3a294303d7c8
Geofrey Wanyama <wanyamag17@gmail.com>
2025-02-22 11:27:53 +0200

    added filtering logs using lambda function

--- training/python/py_logging/py_logging_basics.py
+++ training/python/py_logging/py_logging_basics.py
@@ -14,7 +14,7 @@ stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.ERROR)
 stream_formatter = logging.Formatter("%(asctime)s:%(name)s:%(levelname)s:%(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 stream_handler.setFormatter(stream_formatter)
-# add filter using Filter class
+# Add filter using Filter class
 class FilterLogs(logging.Filter):
     def filter(self, record):
         return "CRITICAL" in record.getMessage()
@@ -24,12 +24,15 @@ logger.addHandler(stream_handler)
 file_handler = logging.FileHandler(f"{__name__}.logs")
 file_handler_formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(funcName)s:%(message)s")
 file_handler.setFormatter(file_handler_formatter)
+# Add filter using lambda function
+file_handler.addFilter(lambda record: "SENSITIVE" not in record.getMessage())
 logger.addHandler(file_handler)
 
 logger.propagate=False
 
 if __name__ == "__main__":
-    logger.info("This is logged to only the log file")
+    logger.info("This is logged to only the log file because it is not sensitive")
+    logger.info("This is not logged anywhere because though it is info, it has SENSITIVE information")
     logger.info("This is logged to only the file because though it is CRITICAL, the level is lower than the set level for streamhandler")
     logger.error("This is logged to only the file because though it is an error, it is not critical")
     logger.error("This is logged to both the console and file because it is an error and it is CRITICAL")
